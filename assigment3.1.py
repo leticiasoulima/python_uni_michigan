743debfaff460601f499e565f5830614007b8856
Leticia Souza Lima <leticia.souza.lima@alumni.usp.br>
2022-04-22 03:49:23 -0300

    Update assigment3.1.py

--- assigment3.1.py
+++ assigment3.1.py
@@ -2,9 +2,6 @@
 # ----Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours.
 #-----Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). 
 
-
-
-
 hr = input('Enter Hours: ')
 try :
     h = float(hr)
